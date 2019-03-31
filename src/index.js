489c0021d16a89182407b588053396465e2e7c01
Bruno <bruno.adamm@gmail.com>
2019-03-31 14:01:21 -0300

    WIP layout

--- src/index.js
+++ src/index.js
@@ -2,15 +2,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 // Create a React component
 //obs: Double quotes for JSX properties and single everywhere else 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Pathfinder"/>
-      <CommentDetail author="Banglalore"/>
-      <CommentDetail author="Wraith"/>
+      <ApprovalCard>
+        <CommentDetail
+          author="Pathfinder"
+          timeAgo="Today at 4:45PM"
+          comment="Nice blog"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Banglalore"
+          timeAgo="Today at 5:45PM"
+          comment="I like the subject"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Wraith"
+          timeAgo="Today at 6:45PM"
+          comment="Great post :)"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
